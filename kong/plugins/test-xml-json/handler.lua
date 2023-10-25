3ece1509b261cea168ed64454b12699c0172ef17
Dipika Shekhawat <dipika.shekhawat@bluealtair.com>
2023-10-25 15:08:37 +0530

    updated plugin

--- kong/plugins/test-xml-json/handler.lua
+++ kong/plugins/test-xml-json/handler.lua
@@ -17,64 +17,63 @@ end
 -- runs in the 'access_by_lua_block'
 function plugin:access(config)
   -- your custom code here
-  kong.service.request.enable_buffering()
-  if config.enable_on_request then
-    function xmlToJsonFunction ()
-      if kong.request.get_header("Content-Type") ~= "application/xml" then
-        local error_response = {
-          message = "XML request body not found",
-        }
-        return kong.response.exit(400, error_response, {
-          ["Content-Type"] = "application/json"
-        })
-      end
-      local initialRequest = kong.request.get_raw_body()
-      local xml = initialRequest
-      local handler = require("xmlhandler.tree")
-      handler = handler:new()
-      --Instantiates the XML parser
-      local parser = xml2lua.parser(handler)
+	kong.service.request.enable_buffering()
+	if config.enable_on_request then
+		function xmlToJsonFunction ()
+		  if kong.request.get_header("Content-Type") ~= "application/xml" then
+			local error_response = {
+			  message = "XML request body not found",
+			}
+			return kong.response.exit(400, error_response, {
+			  ["Content-Type"] = "application/json"
+			})
+		  end
+		  local initialRequest = kong.request.get_raw_body()
+		  local xml = initialRequest
+		  local handler = require("xmlhandler.tree")
+		  handler = handler:new()
+		  --Instantiates the XML parser
+		  local parser = xml2lua.parser(handler)
 
-      parser:parse(xml)
+		  parser:parse(xml)
 
-      -- Function to convert the XML tree to a Lua table recursively
-      local function xml_tree_to_lua_table(xml_tree)
-        local result = {}
-        for tag, value in pairs(xml_tree) do
-          if type(value) == "table" then
-            if #value == 1 and type(value[1]) == "string" then
-              -- Handle single-value elements
-              result[tag] = value[1]
-            else
-              -- Handle nested elements recursively
-              result[tag] = xml_tree_to_lua_table(value)
-            end
-          else
-            -- Handle attributes
-            result[tag] = value
-          end
-        end
-        return result
-      end
+		  -- Function to convert the XML tree to a Lua table recursively
+		  local function xml_tree_to_lua_table(xml_tree)
+			local result = {}
+			for tag, value in pairs(xml_tree) do
+			  if type(value) == "table" then
+				if #value == 1 and type(value[1]) == "string" then
+				  -- Handle single-value elements
+				  result[tag] = value[1]
+				else
+				  -- Handle nested elements recursively
+				  result[tag] = xml_tree_to_lua_table(value)
+				end
+			  else
+				-- Handle attributes
+				result[tag] = value
+			  end
+			end
+			return result
+		  end
 
-      -- Convert the XML tree to a Lua table
-      local lua_table = xml_tree_to_lua_table(handler.root)
-      kong.service.request.set_raw_body(json.encode(lua_table))
-    end
-    function xmlToJsonErrorhandler( err )
-      kong.log.set_serialize_value("request.Xml-To-Json_Request", err)
-      local error_response = {
-        message = "Invalid xml request payload",
-      }
-      return kong.response.exit(400, error_response, {
-        ["Content-Type"] = "application/json"
-      })
-    end
+		  -- Convert the XML tree to a Lua table
+		  local lua_table = xml_tree_to_lua_table(handler.root)
+		  kong.service.request.set_raw_body(json.encode(lua_table))
+		end
+		function xmlToJsonError( err )
+		  kong.log.set_serialize_value("request.Xml-To-Json_Request", err)
+		  local error_response = {
+			message = "Invalid xml request payload",
+		  }
+		  return kong.response.exit(400, error_response, {
+			["Content-Type"] = "application/json"
+		  })
+		end
    
-    status = xpcall( xmlToJsonFunction, xmlToJsonErrorhandler )
-    kong.log.set_serialize_value("request.Xml-To-Json_Request-status", status)
-   
-  end
+		status = xpcall( xmlToJsonFunction, xmlToJsonError )
+		kong.log.set_serialize_value("request.Xml-To-Json_Request-status", status)
+	end
 end
 
 function plugin:header_filter(config)
