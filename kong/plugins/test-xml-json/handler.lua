aa177ca8cfc474f8f83afa87aa687ed9c74dc18f
Dipika Shekhawat <dipika.shekhawat@bluealtair.com>
2023-10-27 15:26:46 +0530

    updated plugin

--- kong/plugins/test-xml-json/handler.lua
+++ kong/plugins/test-xml-json/handler.lua
@@ -1,78 +1,78 @@
-local xml2lua = require("xml2lua")
+  local xml2lua = require("xml2lua")
 
-local json = require "cjson"
+  local json = require "cjson"
 
 
 
-local plugin = {
-  PRIORITY = 803, -- set the plugin priority, which determines plugin execution order
-  VERSION = "0.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
-}
+  local plugin = {
+    PRIORITY = 803, -- set the plugin priority, which determines plugin execution order
+    VERSION = "0.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
+  }
 
-function plugin:rewrite(config)
-  -- Implement logic for the rewrite phase here (http)
-  kong.service.request.enable_buffering()
-end
+  function plugin:rewrite(config)
+    -- Implement logic for the rewrite phase here (http)
+    kong.service.request.enable_buffering()
+  end
 
--- runs in the 'access_by_lua_block'
-function plugin:access(config)
-  -- your custom code here
-	kong.service.request.enable_buffering()
-	if config.enable_on_request then
-		function xmlToJsonFunction ()
-		  if kong.request.get_header("Content-Type") ~= "application/xml" then
-			local error_response = {
-			  message = "XML request body not found",
-			}
-			return kong.response.exit(400, error_response, {
-			  ["Content-Type"] = "application/json"
-			})
-		  end
-		  local initialRequest = kong.request.get_raw_body()
-		  local xml = initialRequest
-		  local handler = require("xmlhandler.tree")
-		  handler = handler:new()
-		  --Instantiates the XML parser
-		  local parser = xml2lua.parser(handler)
+  -- runs in the 'access_by_lua_block'
+  function plugin:access(config)
+    -- your custom code here
+    kong.service.request.enable_buffering()
+    if config.enable_on_request then
+      function xmlToJsonFunction ()
+        if kong.request.get_header("Content-Type") ~= "application/xml" then
+        local error_response = {
+          message = "XML request body not found",
+        }
+        return kong.response.exit(400, error_response, {
+          ["Content-Type"] = "application/json"
+        })
+        end
+        local initialRequest = kong.request.get_raw_body()
+        local xml = initialRequest
+        local handler = require("xmlhandler.tree")
+        handler = handler:new()
+        --Instantiates the XML parser
+        local parser = xml2lua.parser(handler)
 
-		  parser:parse(xml)
-      local lua_table = handler.root
-		  kong.service.request.set_raw_body(json.encode(lua_table))
-		end
-		function xmlToJsonError( err )
-		  kong.log.set_serialize_value("request.Xml-To-Json_Request", err)
-		  local error_response = {
-			message = "Invalid request payload",
-		  }
-		  return kong.response.exit(400, error_response, {
-			["Content-Type"] = "application/json"
-		  })
-		end
-   
-		local status = xpcall( xmlToJsonFunction, xmlToJsonError )
-		kong.log.set_serialize_value("request.Xml-To-Json_Request-status", status)
-	end
-end
+        parser:parse(xml)
+        local lua_table = handler.root
+        kong.service.request.set_raw_body(json.encode(lua_table))
+      end
+      function xmlToJsonError( err )
+        kong.log.set_serialize_value("request.Xml-To-Json_Request", err)
+        local error_response = {
+        message = "Invalid request payload",
+        }
+        return kong.response.exit(400, error_response, {
+        ["Content-Type"] = "application/json"
+        })
+      end
+    
+      local status = xpcall( xmlToJsonFunction, xmlToJsonError )
+      kong.log.set_serialize_value("request.Xml-To-Json_Request-status", status)
+    end
+  end
 
-function plugin:header_filter(config)
-  kong.response.clear_header("Content-Length")
-  kong.response.set_header("Content-Type", "application/json")
-end
+  function plugin:header_filter(config)
+    kong.response.clear_header("Content-Length")
+    kong.response.set_header("Content-Type", "application/json")
+  end
 
-function plugin:body_filter(config)
-  -- Implement logic for the body_filter phase here (http)
-  if config.enable_on_response then
-	if kong.service.response.get_header("Content-Type") == "application/xml" or kong.service.response.get_header("Content-Type") == "application/xml; charset=utf-8"then
-		local initialResponse = kong.service.response.get_raw_body()
-		local xmlResponse = initialResponse
-		local handler = require("xmlhandler.tree")
-		handler = handler:new()
-		local parser = xml2lua.parser(handler)
-		parser:parse(xmlResponse)
-		local response_lua_table = handler.root
-		kong.response.set_raw_body(json.encode(response_lua_table))
-	  end
-	end
-end
--- return our plugin object
-return plugin
+  function plugin:body_filter(config)
+    -- Implement logic for the body_filter phase here (http)
+    if config.enable_on_response then
+    if kong.service.response.get_header("Content-Type") == "application/xml" or kong.service.response.get_header("Content-Type") == "application/xml; charset=utf-8"then
+      local initialResponse = kong.service.response.get_raw_body()
+      local xmlResponse = initialResponse
+      local handler = require("xmlhandler.tree")
+      handler = handler:new()
+      local parser = xml2lua.parser(handler)
+      parser:parse(xmlResponse)
+      local response_lua_table = handler.root
+      kong.response.set_raw_body(json.encode(response_lua_table))
+      end
+    end
+  end
+  -- return our plugin object
+  return plugin
